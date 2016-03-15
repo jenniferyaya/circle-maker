62cd2ef23b1973c41e9a57bba2355ab4da0e824a
Jenniferyaya <jenniferfalanga@Jennifers-MacBook-Pro.local>
2016-03-15 16:43:07 -0400

    Corrected spacing and indentation errors in markbot

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var $body = $('body');
 
-
 $('html').on('keydown', function () {
   var $div = $('<div>');
+
   $body.append($div);
   $div.addClass('ball');
-  $div.css('left', Math.random()*1000);
-  $div.css('top', Math.random()*1500);
+  $div.css('left', Math.random() * 1000);
+  $div.css('top', Math.random() * 1500);
 });
 
